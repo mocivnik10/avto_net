36ac490ddf1e4c0895abcdb9357f9d46b4ffa1c5
mocivnik10 <iztok.mocivnik@poviolabs.com>
2016-06-24 11:47:07 +0200

    Designed index page for cars + new body background image

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,7 +1,14 @@
 class CarsController < ApplicationController
   def index
+    @brand = Array.new
+    @brand[0] = ""
+    Car.all.uniq.each do |c|
+       @brand.push(c.brand)
+    end
+
     #@cars = Car.all
     @search = Car.ransack(params[:q])
+    @search.sorts = 'brand asc' if @search.sorts.empty?
     @cars = @search.result
   end
   def show
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,11 @@
 class WelcomeController < ApplicationController
   def index
+    @brand = Array.new
+    @brand[0] = ""
+    Car.all.uniq.each do |c|
+       @brand.push(c.brand)
+    end
+    
     @search = Car.ransack(params[:q])
     @cars = @search.result
   end
